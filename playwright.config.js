f065373e4dbc1207c2d5af16804cb6e61b268976
Kaio <kaioed@gmail.com>
2026-04-17 23:44:45 +1000

    Update Playwright config and readme links

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/fliptimer',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. */
     trace: 'on-first-retry',
